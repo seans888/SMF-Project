cd59491ed21a6ab2d6087ec59cf9c0d6f3a71813
jgvillacorta <jkevinvillacorta@gmail.com>
2015-07-08 22:18:23 +0800

    testing

--- Practice/Module 2/Draft/SM Scholarship Foundation/backend/views/layouts/main.php
+++ Practice/Module 2/Draft/SM Scholarship Foundation/backend/views/layouts/main.php
@@ -258,7 +258,7 @@ DashboardAsset::register($this);
                       <a href="#" class="btn btn-default btn-flat">Profile</a>
                     </div>
                     <div class="pull-right">
-                      <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                      <a href="/site/logout" class="btn btn-default btn-flat">Sign out</a>
                     </div>
                   </li>
                 </ul>
--- Practice/Module 2/Draft/SM Scholarship Foundation/backend/views/site/login.php
+++ Practice/Module 2/Draft/SM Scholarship Foundation/backend/views/site/login.php
@@ -10,16 +10,32 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
+<div class="login-box">
+      <div class="login-logo">
+        <a href="../../index2.html"><b>Admin</b>LTE</a>
+      </div><!-- /.login-logo -->
+      <div class="login-box-body">
+        <p class="login-box-msg">Sign in to start your session</p>
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <?= $form->field($model, 'username',['options'=>[
+					'tag'=>'div',
+					'class'=>'form-group field-loginform-username has-feedback required'
+				],
+				'template'=>'{input}<span class="glyphicon glyphicon-user form-control-feedback"></span>
+					{error}{hint}'
+				])->textInput(['placeholder'=>'Username']) ?>
+				<?= $form->field($model, 'password',['options'=>[
+					'tag'=>'div',
+					'class'=>'form-group field-loginform-password has-feedback required'
+				],
+				'template'=>'{input}<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+					{error}{hint}'
+				])->passwordInput(['placeholder'=>'Password']) ?>
+                <?= $form->field($model, 'rememberMe',['options'=>[
+					'tag'=>'div',
+					'class'=>'checkbox field-loginform-rememberMe icheck'
+				],
+				])->checkbox() ?>
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
