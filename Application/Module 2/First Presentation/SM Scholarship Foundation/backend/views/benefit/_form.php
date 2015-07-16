069e0722715ce1f59329ee11733045c60effbf70
sudaswani <syam090311@gmail.com>
2015-07-16 21:58:51 +0800

    changed drop down list of scholar id selection to auto search drop down list

--- Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/benefit/_form.php
+++ Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/benefit/_form.php
@@ -5,7 +5,7 @@ use yii\widgets\ActiveForm;
 use common\models\Scholars;
 use common\models\Schools;
 use yii\helpers\ArrayHelper;
-
+use kartik\select2\Select2;
 /* @var $this yii\web\View */
 /* @var $model common\models\Benefit */
 /* @var $form yii\widgets\ActiveForm */
@@ -15,10 +15,13 @@ use yii\helpers\ArrayHelper;
 
     <?php $form = ActiveForm::begin(); ?>
     
-    <?= $form->field($model, 'benefit_scholar_id')->dropDownList(
-    		ArrayHelper::map(Scholars::find()->all(), 'scholar_id','scholar_id','scholar_lastName'),
-    		['prompt'=>'Select Scholar ID']
-    		) ?>
+	<?= $form->field($model,'benefit_scholar_id')->widget(Select2::classname(),
+		[
+			'data'=>ArrayHelper::map(Scholars::find()->all(),'scholar_id','scholar_id','scholar_lastName'),
+			'language'=>'en',
+			'options'=>['placeholder'=>'Select Scholar ID'],
+			'pluginOptions'=>['allowClear'=>true],
+		]) ?>
     		
     <?= $form->field($model, 'benefit_school_id')->dropDownList(
     		ArrayHelper::map(Schools::find()->all(), 'school_id','school_name'),
