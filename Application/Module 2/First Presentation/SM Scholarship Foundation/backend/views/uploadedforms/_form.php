bbe7009c1b5e13e9d98999cd1d0d3a35376d5b23
sudaswani <syam090311@gmail.com>
2015-07-16 19:42:32 +0800

    added the form field for file upload and deleted unnecessary form fields

--- Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/uploadedforms/_form.php
+++ Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/uploadedforms/_form.php
@@ -1,8 +1,9 @@
 <?php
-
+use common\models\Scholars;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use dosamigos\fileinput\FileInput;
 /* @var $this yii\web\View */
 /* @var $model common\models\Uploadedforms */
 /* @var $form yii\widgets\ActiveForm */
@@ -10,17 +11,27 @@ use yii\widgets\ActiveForm;
 
 <div class="uploadedforms-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'scholar_lastName')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'scholar_firstName')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'scholar_middleName')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'uploadedForm')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'scholar_id')->textInput() ?>
+    <?php $form = ActiveForm::begin(['options'=>['enctype'=>'multipart/form-data']]); ?>
+	
+    <?= $form->field($model, 'scholar_id')->dropDownList(
+		ArrayHelper::map(Scholars::find()->all(),'scholar_id','scholar_id','scholar_lastName'),
+		['prompt'=>'Select Scholar ID']
+	)->label('Scholar ID') ?>
+	
+	<?= $form->field($model, 'fileName')->dropDownList(['Registration Form'=>'Registration Form','Grades Form'=>'Grades Form'],
+			['prompt'=>'Select File Type']) ?>
+	
+	<?= $form->field($model, 'file')->widget(\dosamigos\fileinput\BootstrapFileInput::className(), [
+    'options' => ['multiple' => true],
+    'clientOptions' => [
+        'previewFileType' => 'text',
+        'browseClass' => 'btn btn-success',
+        'uploadClass' => 'btn btn-info',
+        'removeClass' => 'btn btn-danger',
+        'removeIcon' => '<i class="glyphicon glyphicon-trash"></i> '
+    ],
+	
+])->label('Upload Form')?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
