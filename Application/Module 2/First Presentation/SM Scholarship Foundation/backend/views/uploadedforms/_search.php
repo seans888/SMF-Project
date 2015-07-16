efe14ed671fc59f2968f873349b0fea16cbcdd13
sudaswani <syam090311@gmail.com>
2015-07-16 19:43:01 +0800

    added a field for searching uploaded form names

--- Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/uploadedforms/_search.php
+++ Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/uploadedforms/_search.php
@@ -27,6 +27,8 @@ use yii\widgets\ActiveForm;
 
     <?php // echo $form->field($model, 'scholar_id') ?>
 
+    <?php // echo $form->field($model, 'fileName') ?>
+
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
