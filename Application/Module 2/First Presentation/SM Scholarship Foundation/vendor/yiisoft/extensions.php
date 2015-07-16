57a2c993b7c5263611ed7cb4d497738d7792f738
sudaswani <syam090311@gmail.com>
2015-07-16 20:11:49 +0800

    added kartik select widget

--- Application/Module 2/First Presentation/SM Scholarship Foundation/vendor/yiisoft/extensions.php
+++ Application/Module 2/First Presentation/SM Scholarship Foundation/vendor/yiisoft/extensions.php
@@ -102,4 +102,22 @@ return array (
       '@dosamigos/fileinput' => $vendorDir . '/2amigos/yii2-file-input-widget/src',
     ),
   ),
+  'kartik-v/yii2-krajee-base' => 
+  array (
+    'name' => 'kartik-v/yii2-krajee-base',
+    'version' => '1.7.7.0',
+    'alias' => 
+    array (
+      '@kartik/base' => $vendorDir . '/kartik-v/yii2-krajee-base',
+    ),
+  ),
+  'kartik-v/yii2-widget-select2' => 
+  array (
+    'name' => 'kartik-v/yii2-widget-select2',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@kartik/select2' => $vendorDir . '/kartik-v/yii2-widget-select2',
+    ),
+  ),
 );
