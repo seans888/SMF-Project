e196b7cdff6e0b34a269d6fabfd08d3d3dc54335
sudaswani <syam090311@gmail.com>
2015-07-16 21:50:51 +0800

    changed drop down list of scholar id selection to auto search drop down list

--- Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/grades/_form.php
+++ Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/grades/_form.php
@@ -3,7 +3,7 @@ use yii\helpers\ArrayHelper;
 use common\models\Scholars;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use kartik\select2\Select2;
 /* @var $this yii\web\View */
 /* @var $model common\models\Grades */
 /* @var $form yii\widgets\ActiveForm */
@@ -13,14 +13,17 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(['options'=>['enctype'=>'multipart/form-data']]); ?>
 
-    <?= $form->field($model, 'grade_schoolYear')->textInput() ?>
+	<?= $form->field($model,'grade_scholar_id')->widget(Select2::classname(),
+		[
+			'data'=>ArrayHelper::map(Scholars::find()->all(),'scholar_id','scholar_id','scholar_lastName'),
+			'language'=>'en',
+			'options'=>['placeholder'=>'Select Scholar ID'],
+			'pluginOptions'=>['allowClear'=>true],
+		]) ?>
+	
+	<?= $form->field($model, 'grade_schoolYear')->textInput() ?>
 
     <?= $form->field($model, 'grade_Term')->textInput() ?>
-
-    <?= $form->field($model, 'grade_scholar_id')->dropDownList(
-    		ArrayHelper::map(Scholars::find()->all(),'scholar_id','scholar_id','scholar_lastName'),
-    		['prompt'=>'Select Scholar ID']
-    		) ?>
 
     <?= $form->field($model, 'grade_value')->textInput(['maxlength' => true]) ?>
 
