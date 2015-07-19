817d90343822b3a4ee6fa4b0fb191ac91359ef74
jlarroyo <jayson.arroyo36@gmail.com>
2015-07-19 16:54:30 +0800

    removed the field for typing the scholar's last name

--- Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/grades/_form.php
+++ Application/Module 2/First Presentation/SM Scholarship Foundation/backend/views/grades/_form.php
@@ -4,6 +4,8 @@ use common\models\Scholars;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\select2\Select2;
+use kartik\typeahead\TypeaheadBasic;
+use kartik\typeahead\Typeahead;
 /* @var $this yii\web\View */
 /* @var $model common\models\Grades */
 /* @var $form yii\widgets\ActiveForm */
@@ -11,7 +13,11 @@ use kartik\select2\Select2;
 
 <div class="grades-form">
 
-    <?php $form = ActiveForm::begin(['options'=>['enctype'=>'multipart/form-data']]); ?>
+    <?php $form = ActiveForm::begin(['options'=>['enctype'=>'multipart/form-data']]);
+		$test = ArrayHelper::map(Scholars::findBySql('SELECT * FROM scholars WHERE scholar_id=2')->all(),'scholar_id','scholar_lastName');
+		$test2 = ArrayHelper::map(Scholars::findBySql('SELECT * FROM scholars WHERE scholar_id=3')->all(),'scholar_id','scholar_lastName');
+		$test3 = array_values($test2)[0].' '.array_values($test)[0];
+	?>
 
 	<?= $form->field($model,'grade_scholar_id')->widget(Select2::classname(),
 		[
@@ -25,8 +31,7 @@ use kartik\select2\Select2;
 
     <?= $form->field($model, 'grade_Term')->textInput() ?>
 
-    <?= $form->field($model, 'grade_value')->textInput(['maxlength' => true]) ?>
-
+	<?= $form->field($model, 'grade_value')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
