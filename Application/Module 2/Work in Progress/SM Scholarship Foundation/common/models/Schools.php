8578eec0df6ac012cbd0921bf3440d75b7c20d95
sudaswani <syam090311@gmail.com>
2015-07-20 22:38:59 +0800

    removed school_id error

--- Application/Module 2/Work in Progress/SM Scholarship Foundation/common/models/Schools.php
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/common/models/Schools.php
@@ -7,14 +7,18 @@ use Yii;
 /**
  * This is the model class for table "schools".
  *
- * @property integer $school_id
+ * @property integer $School_id
  * @property string $school_name
  * @property string $school_area
  * @property string $school_address
  * @property string $school_contactEmail
  * @property integer $school_contactNumber
  *
+ * @property Allowance[] $allowances
+ * @property Benefit[] $benefits
  * @property Compile[] $compiles
+ * @property Equivalence[] $equivalences
+ * @property Failures[] $failures
  * @property Scholars[] $scholars
  */
 class Schools extends \yii\db\ActiveRecord
@@ -34,7 +38,6 @@ class Schools extends \yii\db\ActiveRecord
     {
         return [
             [['school_name', 'school_area', 'school_contactNumber'], 'required'],
-			[['school_contactEmail'],'email'],
             [['school_area'], 'string'],
             [['school_contactNumber'], 'integer'],
             [['school_name', 'school_address', 'school_contactEmail'], 'string', 'max' => 100]
@@ -47,13 +50,29 @@ class Schools extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'school_id' => 'School ID',
+            'School_id' => 'School ID',
             'school_name' => 'School Name',
-            'school_area' => 'Area',
-            'school_address' => 'Address',
-            'school_contactEmail' => 'Contact Email',
-            'school_contactNumber' => 'Contact Number',
+            'school_area' => 'School Area',
+            'school_address' => 'School Address',
+            'school_contactEmail' => 'School Contact Email',
+            'school_contactNumber' => 'School Contact Number',
         ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAllowances()
+    {
+        return $this->hasMany(Allowance::className(), ['allowance_school_id' => 'school_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBenefits()
+    {
+        return $this->hasMany(Benefit::className(), ['benefit_school_id' => 'school_id']);
     }
 
     /**
@@ -62,6 +81,22 @@ class Schools extends \yii\db\ActiveRecord
     public function getCompiles()
     {
         return $this->hasMany(Compile::className(), ['compile_school_id' => 'school_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEquivalences()
+    {
+        return $this->hasMany(Equivalence::className(), ['School_id' => 'school_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFailures()
+    {
+        return $this->hasMany(Failures::className(), ['fail_school_id' => 'school_id']);
     }
 
     /**
