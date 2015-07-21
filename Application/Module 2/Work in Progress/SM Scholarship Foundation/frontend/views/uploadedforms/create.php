bd17e437183b130e197f93ffae7d484d52850757
jgvillacorta <jkevinvillacorta@gmail.com>
2015-07-21 23:20:01 +0800

    Upload for frontend working

--- Application/Module 2/Work in Progress/SM Scholarship Foundation/frontend/views/uploadedforms/create.php
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/frontend/views/uploadedforms/create.php
@@ -6,13 +6,12 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Uploadedforms */
 
-$this->title = 'Create Uploadedforms';
-$this->params['breadcrumbs'][] = ['label' => 'Uploadedforms', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Upload Forms';
+
 ?>
 <div class="uploadedforms-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 style="margin-top:100px;"><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
