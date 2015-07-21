febff79a7fc37662082837ac2b034007c0cbce4e
sudaswani <syam090311@gmail.com>
2015-07-21 18:52:33 +0800

    created _uploadedformsrecords.php which is the content of the dropped down item

---
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/uploadedforms/_uploadedformsrecords.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\UploadedformsSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Uploadedforms';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="uploadedforms-index">
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+  //          'uploaded_scholar_id',
+			[
+				'attribute'=>'scholar_lastName',
+				'value'=>'scholar.scholar_lastName',
+			],
+			[
+				'attribute'=>'scholar_firstName',
+				'value'=>'scholar.scholar_firstName',
+			],
+			[
+				'attribute'=>'scholar_middleName',
+				'value'=>'scholar.scholar_middleName',
+			],
+            'uploadedForm',
+            'fileName',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
