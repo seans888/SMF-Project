5d01c9a8579e2bbd60d7b771b5a1c5332974ae7b
sudaswani <syam090311@gmail.com>
2015-07-21 23:34:11 +0800

    created kartik gridview for benefits

--- Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/benefit/index.php
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/benefit/index.php
@@ -1,46 +1,75 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
-
+use kartik\grid\GridView;
+use common\models\BenefitSearch;
 /* @var $this yii\web\View */
-/* @var $searchModel common\models\BenefitSearch */
+/* @var $searchModel common\models\ScholarsSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Benefits';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="benefit-index">
+<div class="scholars-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Benefit', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Input Benefits', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
+    <br>
+	<p><b><font color=orange>Orange</font> rows are scholars from NCR Areas</p>
+	<p><font color=blue>Blue</font> rows are scholars from Provincial Areas</b>
+	</p>
+    
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+    	'rowOptions'=>function($model){
+    		if($model->scholar_school_area=='Provincial')
+    		{
+    			return['class'=>'alert-info'];
+    		}
+    		else if($model->scholar_school_area=='NCR')
+    		{
+    			return['class'=>'alert-warning'];
+    		}
+    	},
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            [
+				'class' => 'kartik\grid\ExpandRowColumn',
+				'value' => function($model, $key, $index, $column){
+					return GridView::ROW_COLLAPSED;
+				},
+				'detail' => function ($model, $key, $index, $column){
+					$searchModel = new BenefitSearch();
+					$searchModel -> benefit_scholar_id = $model->scholar_id;
+					$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+					
+					return Yii::$app->controller->renderPartial('_benefitsrecords',[
+						'searchModel' => $searchModel,
+						'dataProvider' => $dataProvider,
+					]);
+				},
+			],
 
-            'benefit_scholar_id',
-        		[
-        		'attribute'=>'benefit_scholar_lastName',
-        		'value'=>'benefitScholar.scholar_lastName',
-        		],
-        		[
-        		'attribute'=>'benefit_scholar_firstName',
-        		'value'=>'benefitScholar.scholar_firstName',
-        		],
-        		[
-        		'attribute'=>'benefit_scholar_middleName',
-        		'value'=>'benefitScholar.scholar_middleName',
-        		],        
-            'benefit_amount',
-            'benefit_scholarShare',
-			'benefit_description',
+            'scholar_id',
+            'scholar_firstName',
+            'scholar_lastName',
+            'scholar_middleName',
+            // 'scholar_gender',
+            // 'scholar_address',
+            [
+            	'attribute'=>'scholar_school_id',
+            	'value'=>'scholarSchool.school_name',
+            ],
+            'scholar_course',
+            'scholar_yearLevel',
+            // 'scholar_email:email',
+            // 'scholar_contactNum',
+            // 'scholar_cashCardNum',
+         // 'scholar_school_area',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
