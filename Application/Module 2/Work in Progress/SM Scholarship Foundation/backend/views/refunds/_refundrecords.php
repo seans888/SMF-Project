243e970532e0305f78175a34dd6aa99f47b3fc38
jlarroyo <jayson.arroyo36@gmail.com>
2015-07-25 19:13:39 +0800

    created what is inside the expansion

---
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/refunds/_refundrecords.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\RefundsSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Refunds';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="refunds-index">
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'refund_id',
+            'refund_amount',
+            'refund_smShare',
+            'refund_scholarShare',
+            // 'refund_scholar_id',
+            // 'refund_tuitionfee_id',
+            // 'refund_description',
+            // 'refund_date',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
