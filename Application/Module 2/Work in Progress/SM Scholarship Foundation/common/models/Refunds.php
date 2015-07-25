193404f8e613ff3fd4e8384a6d2eccb8f778c141
sudaswani <syam090311@gmail.com>
2015-07-25 16:02:33 +0800

    added Refunds.php model using gii

---
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/common/models/Refunds.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "refunds".
+ *
+ * @property integer $refund_id
+ * @property string $refund_amount
+ * @property string $refund_smShare
+ * @property string $refund_scholarShare
+ * @property integer $refund_scholar_id
+ * @property integer $refund_tuitionfee_id
+ * @property string $refund_description
+ * @property string $refund_date
+ */
+class Refunds extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'refunds';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['refund_amount', 'refund_smShare', 'refund_scholarShare', 'refund_scholar_id', 'refund_tuitionfee_id', 'refund_description', 'refund_date'], 'required'],
+            [['refund_amount', 'refund_smShare', 'refund_scholarShare'], 'number'],
+            [['refund_scholar_id', 'refund_tuitionfee_id'], 'integer'],
+            [['refund_date'], 'safe'],
+            [['refund_description'], 'string', 'max' => 100]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'refund_id' => 'Refund ID',
+            'refund_amount' => 'Refund Amount',
+            'refund_smShare' => 'Refund Sm Share',
+            'refund_scholarShare' => 'Refund Scholar Share',
+            'refund_scholar_id' => 'Refund Scholar ID',
+            'refund_tuitionfee_id' => 'Refund Tuitionfee ID',
+            'refund_description' => 'Refund Description',
+            'refund_date' => 'Refund Date',
+        ];
+    }
+}
