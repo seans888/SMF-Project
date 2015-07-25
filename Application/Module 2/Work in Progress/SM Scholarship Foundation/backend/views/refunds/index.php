806190d09a20b53e6318dcd8672ac901131e1c38
jlarroyo <jayson.arroyo36@gmail.com>
2015-07-25 19:13:14 +0800

    created expansion grid view

--- Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/refunds/index.php
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/refunds/index.php
@@ -1,38 +1,75 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
-
+use kartik\grid\GridView;
+use common\models\RefundsSearch;
 /* @var $this yii\web\View */
-/* @var $searchModel common\models\RefundsSearch */
+/* @var $searchModel common\models\ScholarsSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Refunds';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="refunds-index">
+<div class="scholars-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Refunds', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Input Refunds', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
+    <br>
+	<p><b><font color=orange>Orange</font> rows are scholars from NCR Areas</p>
+	<p><font color=blue>Blue</font> rows are scholars from Provincial Areas</b>
+	</p>
+    
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+    	'rowOptions'=>function($model){
+    		if($model->scholar_school_area=='Provincial')
+    		{
+    			return['class'=>'alert-info'];
+    		}
+    		else if($model->scholar_school_area=='NCR')
+    		{
+    			return['class'=>'alert-warning'];
+    		}
+    	},
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            [
+				'class' => 'kartik\grid\ExpandRowColumn',
+				'value' => function($model, $key, $index, $column){
+					return GridView::ROW_COLLAPSED;
+				},
+				'detail' => function ($model, $key, $index, $column){
+					$searchModel = new RefundsSearch();
+					$searchModel -> refund_scholar_id = $model->scholar_id;
+					$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+					
+					return Yii::$app->controller->renderPartial('_refundrecords',[
+						'searchModel' => $searchModel,
+						'dataProvider' => $dataProvider,
+					]);
+				},
+			],
 
-            'refund_id',
-            'refund_amount',
-            'refund_smShare',
-            'refund_scholarShare',
-            'refund_scholar_id',
-            // 'refund_tuitionfee_id',
-            // 'refund_description',
-            // 'refund_date',
+            'scholar_id',
+            'scholar_firstName',
+            'scholar_lastName',
+            'scholar_middleName',
+            // 'scholar_gender',
+            // 'scholar_address',
+            [
+            	'attribute'=>'scholar_school_id',
+            	'value'=>'scholarSchool.school_name',
+            ],
+            'scholar_course',
+            'scholar_yearLevel',
+            // 'scholar_email:email',
+            // 'scholar_contactNum',
+            // 'scholar_cashCardNum',
+         // 'scholar_school_area',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
