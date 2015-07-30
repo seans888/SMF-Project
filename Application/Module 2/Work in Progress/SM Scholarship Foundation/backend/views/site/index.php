d50f423be07af8cc41ea0ec31cc40ce6d053e691
sudaswani <syam090311@gmail.com>
2015-07-30 18:57:19 +0800

    changed the layout style of the colored boxes

--- Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/site/index.php
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Welcome Admin!</h1>
+        <center><h1>Welcome Admin!</h1></center>
 
     <div class="body-content">
 	
@@ -18,51 +18,57 @@ $this->title = 'My Yii Application';
               <div class="small-box bg-aqua">
                 <div class="inner">
                   <h3>Upload</h3>
+				  <br><br>
                   <p>Upload Requirements</p>
                 </div>
                 <div class="icon">
                   <i class="fa fa-file-o"></i>
                 </div>
-                <a href="<?= Yii::$app->getUrlManager()->createUrl('/uploadedforms/create'); ?>" class="small-box-footer">Upload Now<i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?= Yii::$app->getUrlManager()->createUrl('/uploadedforms/create'); ?>" class="small-box-footer">Upload Now&nbsp&nbsp&nbsp<i class="fa fa-arrow-circle-right"></i></a>
               </div>
             </div><!-- ./col -->
             <div class="col-lg-3 col-xs-6">
               <!-- small box -->
               <div class="small-box bg-green">
                 <div class="inner">
-                  <h3>Make Report</h3>
+                  <h3>Report</h3>
+				  <br><br>
                   <p>List of Reports</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="<?= Yii::$app->getUrlManager()->createUrl('/gpa_report/index'); ?>" class="small-box-footer">Select a Report to Create<i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?= Yii::$app->getUrlManager()->createUrl('/gpa_report/index'); ?>" class="small-box-footer">Select a Report to Create&nbsp&nbsp&nbsp<i class="fa fa-arrow-circle-right"></i></a>
               </div>
             </div><!-- ./col -->
+			</div><!-- ./row -->
+			<div class="row">
             <div class="col-lg-3 col-xs-6">
               <!-- small box -->
               <div class="small-box bg-yellow">
                 <div class="inner">
-                  <h3>Add</h3>
+                  <h3>Scholar</h3>
+				  <br><br>
                   <p>Add scholar</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-person-add"></i>
                 </div>
-                <a href="<?= Yii::$app->getUrlManager()->createUrl('/scholars/create'); ?>" class="small-box-footer">Add a Scholar Record Now<i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?= Yii::$app->getUrlManager()->createUrl('/scholars/create'); ?>" class="small-box-footer">Add a Scholar Record Now&nbsp&nbsp&nbsp<i class="fa fa-arrow-circle-right"></i></a>
               </div>
             </div><!-- ./col -->
             <div class="col-lg-3 col-xs-6">
               <!-- small box -->
               <div class="small-box bg-red">
                 <div class="inner">
-                  <h3>Check Calendar</h3>
+                  <h3>Calendar</h3>
+				  <br><br>
                   <p>Check the Calendar of events</p>
                 </div>
                 <div class="icon">
                   <i class="fa fa-calendar"></i>
                 </div>
-                <a href="<?= Yii::$app->getUrlManager()->createUrl('/event/index'); ?>" class="small-box-footer">Check Calendar Now<i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?= Yii::$app->getUrlManager()->createUrl('/event/index'); ?>" class="small-box-footer">Check Calendar Now&nbsp&nbsp&nbsp<i class="fa fa-arrow-circle-right"></i></a>
               </div>
             </div><!-- ./col -->
           </div><!-- /.row -->
