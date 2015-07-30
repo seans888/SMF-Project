10b1111ff24c3aca6c8247d5f17be598b0fdc55e
sudaswani <syam090311@gmail.com>
2015-07-30 21:52:33 +0800

    Added Pjax search, changed color for scholars on NCR and Provincial. Changed the create button to blue instead of green

--- Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/deductions/_deductionrecords.php
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/deductions/_deductionrecords.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
-
+use yii\widgets\Pjax;
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\DeductionsSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -11,7 +11,7 @@ $this->title = 'Deductions';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="deductions-index">
-
+    <?php Pjax::begin(['timeout'=>10000]); ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -27,5 +27,5 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
-
+    <?php Pjax::end(); ?>
 </div>
