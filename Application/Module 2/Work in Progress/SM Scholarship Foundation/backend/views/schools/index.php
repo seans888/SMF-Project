826f6a783b5cc00e498fa31b95b8773ffc90ffc4
sudaswani <syam090311@gmail.com>
2015-08-02 20:31:55 +0800

    changed color for ncr and provincial filtering. uses kartik gridView for auto adjust for columns

--- Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/schools/index.php
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/backend/views/schools/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\SchoolsSearch */
@@ -18,12 +18,24 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a('Create Schools', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
+    <br>
+	<p><b><font color=#988db2>Orange</font> rows are schools from NCR Areas</p>
+	<p><font color=#e7bd58>Purple</font> rows are schools from Provincial Areas</b>
+	</p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+    	'rowOptions'=>function($model){
+    		if(strcasecmp($model->school_area, 'NCR') != 0)
+    		{
+    			return['class'=>'provincial-row'];
+    		}
+    		else if(strcasecmp($model->school_area, 'NCR') == 0)
+    		{
+    			return['class'=>'ncr-row'];
+    		}
+    	},
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
 
  //           'School_id',
             'school_name',
