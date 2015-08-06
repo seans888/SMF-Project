f84a15d45e77798cf154731fa666fd7f5bc3de38
John Michael Santos <jhnmchlsantos@gmail.com>
2015-08-06 20:53:27 +0800

    Added a datepicker for the events date field but error in creating events

--- Application/Module 3/SMF-ATS/advanced/frontend/views/event/_form.php
+++ Application/Module 3/SMF-ATS/advanced/frontend/views/event/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use dosamigos\datepicker\DatePicker;
 /* @var $this yii\web\View */
 /* @var $model app\models\Event */
 /* @var $form yii\widgets\ActiveForm */
@@ -18,7 +18,17 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'event_descript')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'event_date')->textInput() ?>
+    <?= $form->field($model, 'event_date')->widget(
+    DatePicker::className(), [
+        // inline too, not bad
+         'inline' => false, 
+         // modify template for custom rendering
+        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+        'clientOptions' => [
+            'autoclose' => true,
+            'format' => 'yyyy-m-d'
+        ]
+]);?>
 
     <?= $form->field($model, 'event_place')->textInput(['maxlength' => true]) ?>
 
