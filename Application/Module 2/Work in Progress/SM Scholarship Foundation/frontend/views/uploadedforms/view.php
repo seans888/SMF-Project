e955576565e688aad0311deca400783f48e9bc14
jgvillacorta <jkevinvillacorta@gmail.com>
2015-08-10 20:36:23 +0800

    edited for grades error

--- Application/Module 2/Work in Progress/SM Scholarship Foundation/frontend/views/uploadedforms/view.php
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/frontend/views/uploadedforms/view.php
@@ -11,7 +11,7 @@ $this->title = 'Updates';
 <div class="uploadedforms-view">
 
     <h1 style="margin-top:100px;"><?= Html::encode($this->title) ?></h1>
-	<?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+	
 	<p style="margin-top:50px;">
 	
     <?= DetailView::widget([
