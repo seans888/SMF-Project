cd2cd5f36b5ad2547a591556f789948b8e4c8607
jgvillacorta <jkevinvillacorta@gmail.com>
2015-08-11 15:05:39 +0800

    Part time job request form

---
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/frontend/views/parttimejobs/index.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\ParttimejobsSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Part Time Jobs';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="parttimejobs-index">
+	<center style="margin-top:100px;"><h1>Part-Time Job Request History</h1><br> <p>
+        <?= Html::a('Create Job Request', ['create'], ['class' => 'btn btn-success']) ?>
+    </p></center>
+	
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'job_location',
+            'job_startDate',
+            'job_endDate',
+            'job_description:ntext',
+
+            
+        ],
+    ]); ?>
+
+</div>
