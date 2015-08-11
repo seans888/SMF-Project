9a22e97862f808714cf60b7cd62c99759faa2de9
jgvillacorta <jkevinvillacorta@gmail.com>
2015-08-11 19:23:15 +0800

    configured for sending email

---
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/common/models/Emails.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "emails".
+ *
+ * @property integer $id
+ * @property string $receiver_name
+ * @property string $receiver_email
+ * @property string $subject
+ * @property string $content
+ */
+class Emails extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'emails';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['receiver_name', 'receiver_email', 'subject', 'content'], 'required'],
+            [['content'], 'string'],
+            [['receiver_name'], 'string', 'max' => 100],
+            [['receiver_email'], 'string', 'max' => 200],
+            [['subject'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'receiver_name' => 'Receiver Name',
+            'receiver_email' => 'Receiver Email',
+            'subject' => 'Subject',
+            'content' => 'Content',
+        ];
+    }
+}
