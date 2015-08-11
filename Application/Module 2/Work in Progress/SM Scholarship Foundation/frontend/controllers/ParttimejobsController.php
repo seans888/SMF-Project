923346dcfc96905267c98d071fb6e71f899da099
jgvillacorta <jkevinvillacorta@gmail.com>
2015-08-11 16:07:09 +0800

    ParttimejobsController

---
+++ Application/Module 2/Work in Progress/SM Scholarship Foundation/frontend/controllers/ParttimejobsController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\Parttimejobs;
+use common\models\ParttimejobsSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use common\models\Scholars;
+use common\models\User;
+
+/**
+ * ParttimejobsController implements the CRUD actions for Parttimejobs model.
+ */
+class ParttimejobsController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Parttimejobs models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+		$username=Yii::$app->user->identity->username;
+		$users = User::find()->all();
+		$scholars = Scholars::find()->all();
+		$model = new Parttimejobs();
+		
+		foreach($users as $user){
+			foreach($scholars as $scholar){
+				if($user->username==$username&&$user->id==$scholar->scholar_id){
+					$model->job_scholar_id=$scholar->scholar_id;
+					 $searchModel = new ParttimejobsSearch($model);
+					$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+					return $this->render('index', [
+					'searchModel' => $searchModel,
+					'dataProvider' => $dataProvider,
+					]);
+				}
+			}
+		}
+        
+    }
+
+    /**
+     * Displays a single Parttimejobs model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Parttimejobs model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+		$username=Yii::$app->user->identity->username;
+		$users = User::find()->all();
+		$scholars = Scholars::find()->all();
+		$model = new Parttimejobs();
+		
+		foreach($users as $user){
+			foreach($scholars as $scholar){
+				if($user->username==$username&&$user->id==$scholar->scholar_id){
+					$model->job_scholar_id=$scholar->scholar_id;
+	
+					if ($model->load(Yii::$app->request->post()) && $model->save()) {
+					return $this->redirect(['index', 'id' => $model->id]);
+					} else {
+					return $this->render('create', [
+					'model' => $model,
+					]);
+					}
+				}
+			}
+		}
+      
+		
+    }
+
+    /**
+     * Updates an existing Parttimejobs model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Parttimejobs model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Parttimejobs model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Parttimejobs the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Parttimejobs::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
