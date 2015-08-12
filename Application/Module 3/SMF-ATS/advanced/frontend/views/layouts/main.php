c7d9bb4d08aa07b8b30e1165ba41a95c2c2f3cb2
John Michael Santos <jhnmchlsantos@gmail.com>
2015-08-12 17:49:47 +0800

    Fixed in adding a new user in sign up by admin

--- Application/Module 3/SMF-ATS/advanced/frontend/views/layouts/main.php
+++ Application/Module 3/SMF-ATS/advanced/frontend/views/layouts/main.php
@@ -40,7 +40,7 @@ AppAsset::register($this);
 				
             ];
             if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+               // $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
                 $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
             } else {
 
