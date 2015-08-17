293d0259e4883679418cf1e554985c1dc12cc7f7
sudaswani <syam090311@gmail.com>
2015-08-17 18:24:00 +0800

    added panel and export function for scholars

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/scholar/index.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/scholar/index.php
@@ -1,7 +1,49 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use kartik\export\ExportMenu;
+
+$exportedValues = 
+[            
+	['class' => 'kartik\grid\SerialColumn'],
+
+		'scholar_id',
+		'school_school_id',
+		'scholar_first_name',
+		'scholar_middle_name',
+		'scholar_last_name',
+		'scholar_gender',
+		'scholar_address',
+		'scholar_course',
+		'scholar_graduate_status',
+		'scholar_year_level',
+		'scholar_contact_email:email',
+		'scholar_contact_number',
+		'scholar_allowance_status',
+		'scholar_cash_card_number',
+		'scholar_type',
+		'scholar_sponsor',
+		'allowance_allowance_area',
+
+	['class' => 'kartik\grid\ActionColumn'],
+];
+
+$export = ExportMenu::widget([
+		'dataProvider' => $dataProvider,
+        'columns' => $exportedValues,
+		'noExportColumns'=>[18],
+        'columnSelectorOptions'=>[
+            'label' => 'Columns',
+            'class' => 'btn btn-danger'
+        ],
+		'target' => '_blank',
+        'fontAwesome' => true,
+        'dropdownOptions' => [
+            'label' => 'Export',
+            'class' => 'btn btn-success'
+        ]
+	]);
 
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\ScholarSearch */
@@ -22,29 +64,26 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'scholar_id',
-            'school_school_id',
-            'scholar_first_name',
-            'scholar_middle_name',
-            'scholar_last_name',
-            // 'scholar_gender',
-            // 'scholar_address',
-            // 'scholar_course',
-            // 'scholar_graduate_status',
-            // 'scholar_year_level',
-            // 'scholar_contact_email:email',
-            // 'scholar_contact_number',
-            // 'scholar_allowance_status',
-            // 'scholar_cash_card_number',
-            // 'scholar_type',
-            // 'scholar_sponsor',
-            // 'allowance_allowance_area',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+        'columns' => $exportedValues,
+		'toolbar'=> [
+			[
+				'content'=>Html::a('Create Scholar', ['create'], ['class' => 'btn btn-success'])
+			],
+  //      	'{export}',
+			'{toggleData}',
+			$export
+		],
+    // set export properties
+    // 'export'=>[
+        // 'fontAwesome'=>true,
+		// 'label' => 'Export',
+		// 'target' => '_blank'
+    // ],
+		'panel'=>[
+        'type'=>GridView::TYPE_PRIMARY,
+        'heading'=>'Scholar List',
+    ],
+    ]); 
+	?>
 
 </div>
