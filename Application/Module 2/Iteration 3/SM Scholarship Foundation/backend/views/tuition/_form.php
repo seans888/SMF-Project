18756b925a193a79671c0591006d0e33cd9df779
jlarroyo <jayson.arroyo36@gmail.com>
2015-08-21 21:07:37 +0800

    added date picker widget for enrollment and payment date

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/tuition/_form.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/tuition/_form.php
@@ -1,5 +1,5 @@
 <?php
-
+use dosamigos\datepicker\DatePicker;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\select2\Select2;
@@ -28,13 +28,33 @@ use common\models\Scholar;
 
     <?= $form->field($model, 'tuition_school_year_end')->textInput() ?>
 
-    <?= $form->field($model, 'tuition_enrollment_date')->textInput() ?>
+    <?= $form->field($model, 'tuition_enrollment_date')->widget(
+			DatePicker::className(), [
+				// inline too, not bad
+				 'inline' => false, 
+				 // modify template for custom rendering
+			//	'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+				'clientOptions' => [
+					'autoclose' => true,
+					'format' => 'yyyy-mm-dd'
+				]
+			]);?>
 
     <?= $form->field($model, 'tuition_amount')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'tuition_paid_status')->dropDownList([ 'Paid' => 'Paid', 'Not Paid' => 'Not Paid', ], ['prompt' => '']) ?>
 
-    <?= $form->field($model, 'tuition_payment_date')->textInput() ?>
+    <?= $form->field($model, 'tuition_payment_date')->widget(
+			DatePicker::className(), [
+				// inline too, not bad
+				 'inline' => false, 
+				 // modify template for custom rendering
+			//	'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+				'clientOptions' => [
+					'autoclose' => true,
+					'format' => 'yyyy-mm-dd'
+				]
+			]);?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
