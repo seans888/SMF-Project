436e6a9e75dfbac91ffe99b916a467ac50de7edc
sudaswani <syam090311@gmail.com>
2015-08-21 23:21:05 +0800

    fixed minor $query error

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/common/models/TuitionSearch.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/common/models/TuitionSearch.php
@@ -19,7 +19,7 @@ class TuitionSearch extends Tuition
     {
         return [
             [['tuition_id', 'scholar_scholar_id', 'scholar_school_school_id', 'tuition_term', 'tuition_school_year_start', 'tuition_school_year_end'], 'integer'],
-            [['tuition_enrollment_date', 'tuition_paid_status', 'tuition_payment_date'], 'safe'],
+            [['firstName','middleName','lastName','tuition_enrollment_date', 'tuition_paid_status', 'tuition_payment_date'], 'safe'],
             [['tuition_amount'], 'number'],
         ];
     }
@@ -56,6 +56,8 @@ class TuitionSearch extends Tuition
             return $dataProvider;
         }
 
+		$query->joinWith('scholarScholar');
+		
         $query->andFilterWhere([
             'tuition_id' => $this->tuition_id,
             'scholar_scholar_id' => $this->scholar_scholar_id,
@@ -68,7 +70,10 @@ class TuitionSearch extends Tuition
             'tuition_payment_date' => $this->tuition_payment_date,
         ]);
 
-        $query->andFilterWhere(['like', 'tuition_paid_status', $this->tuition_paid_status]);
+        $query->andFilterWhere(['like', 'tuition_paid_status', $this->tuition_paid_status])
+		->andFilterWhere(['like', 'scholar.scholar_first_name', $this->firstName])
+		->andFilterWhere(['like', 'scholar.scholar_middle_name', $this->middleName])
+		->andFilterWhere(['like', 'scholar.scholar_last_name', $this->lastName]);
 
         return $dataProvider;
     }
