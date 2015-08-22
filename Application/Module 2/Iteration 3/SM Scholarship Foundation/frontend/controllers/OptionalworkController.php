e9e4904c9cd5861a05ca66316897d980a3a2ce77
jgvillacorta <jkevinvillacorta@gmail.com>
2015-08-23 00:33:03 +0800

    changed for requirements

---
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/frontend/controllers/OptionalworkController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\Optionalwork;
+use common\models\OptionalworkSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use common\models\Scholar;
+use common\models\User;
+
+/**
+ * OptionalworkController implements the CRUD actions for Optionalwork model.
+ */
+class OptionalworkController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Optionalwork models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        	$username=Yii::$app->user->identity->username;
+		$users = User::find()->all();
+		$scholars = Scholar::find()->all();
+		$model = new Optionalwork();
+		
+		foreach($users as $user){
+			foreach($scholars as $scholar){
+				if($user->username==$username&&$user->id==$scholar->scholar_id){
+					$model->scholar_scholar_id=$scholar->scholar_id;
+					 $searchModel = new OptionalworkSearch($model);
+					$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+					return $this->render('index', [
+					'searchModel' => $searchModel,
+					'dataProvider' => $dataProvider,
+					]);
+					
+				}
+
+			}
+		}
+    }
+
+    /**
+     * Displays a single Optionalwork model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Optionalwork model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+      	$username=Yii::$app->user->identity->username;
+		$users = User::find()->all();
+		$scholars = Scholar::find()->all();
+		$model = new Optionalwork();
+		
+		foreach($users as $user){
+			foreach($scholars as $scholar){
+				if($user->username==$username&&$user->id==$scholar->scholar_id){
+					$model->scholar_scholar_id=$scholar->scholar_id;
+	
+					if ($model->load(Yii::$app->request->post()) && $model->save()) {
+					return $this->redirect(['index', 'id' => $model->id]);
+					} else {
+					return $this->render('create', [
+					'model' => $model,
+					]);
+					}
+				}
+			}
+		}
+      
+		
+    }
+
+    /**
+     * Updates an existing Optionalwork model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->optionalwork_id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Optionalwork model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Optionalwork model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Optionalwork the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Optionalwork::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
