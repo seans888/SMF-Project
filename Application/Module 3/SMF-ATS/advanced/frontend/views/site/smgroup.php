80c3461124a88fb1efc4d422c200181a94515617
John Michael Santos <jhnmchlsantos@gmail.com>
2015-08-24 19:49:26 +0800

    Fix the SM Group page

--- Application/Module 3/SMF-ATS/advanced/frontend/views/site/smgroup.php
+++ Application/Module 3/SMF-ATS/advanced/frontend/views/site/smgroup.php
@@ -13,21 +13,21 @@ $this->title = 'SM Foundation Alumni Tracking System';
         <div class="row">
             <div class="col-lg-4">
                 <h2>SM Department Store</h2>
-				<img src = "images/alumni1.jpg" height="150px" width="250px">
+				<img src = "images/smstore.jpg" height="150px" width="250px">
                 <p>is the country's leading one-stop lifestyle and fashion chain. For many decades now, and for countless shoppers, 
 				it offers the widest array of quality products at affordable prices, truly the epitome of "value for money" shopping. </p>
 
                 </div>
             <div class="col-lg-4">
                 <h2>SM Supermarket</h2>
-				<img src = "images/news.jpg" height="150px" width="250px">
+				<img src = "images/smsuper.jpg" height="150px" width="250px">
                 <p>is the country's top supermarket chain. With an extensive, nation-wide presence and employing state of the art retailing 
 				technology, it offers both food and non-food products, with much emphasis on quality, convenience, and affordability. </p>
 
                 </div>
             <div class="col-lg-4">
                 <h2>SM Hypermarket</h2>
-				<img src = "images/calendar.jpg" height="150px" width="250px">
+				<img src = "images/smhyper.png" height="150px" width="250px">
                 <p> is an innovative, rapidly-growing retail chain, offering unbeatable convenience through its targeted balance of wet 
 				market, grocery, general merchandise, and discount products, all under one roof.</p>
 
@@ -38,21 +38,21 @@ $this->title = 'SM Foundation Alumni Tracking System';
 	<div class="row">
             <div class="col-lg-4">
                 <h2>SM Prime Holdings, Inc</h2>
-				<img src = "images/alumni1.jpg" height="150px" width="250px">
+				<img src = "images/smprime.png" height="150px" width="250">
                 <p>is the Philippines' largest mall operator. Publicly listed since 1994, it now owns and runs world-class malls all over 
 				the country, providing millions of square meters of floor area for a fully integrated shopping, dining, and entertainment experience </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>Banco De Oro</h2>
-				<img src = "images/news.jpg" height="150px" width="250px">
+				<img src = "images/bdo.jpg" height="150px" width="250px">
                 <p> is among the country's top five banks in terms of resources. It is a full-service universal bank providing a wide range of corporate, 
 				commercial, retail, and investment banking services through its more than 200 branches in key business and commercial areas, nationwide. </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>Chinabank</h2>
-				<img src = "images/calendar.jpg" height="150px" width="250px">
+				<img src = "images/chinabank.jpg" height="150px" width="250px">
                 <p> is the first privately-owned local commercial bank in the Philippines. It was incorporated and started operations in 1920 and went public in 
 				1965. Now a universal bank, it provides, among others, deposits and related services; international banking services; insurance products; loans and 
 				credit facilities; and trust and investment services. </p>
@@ -65,20 +65,20 @@ $this->title = 'SM Foundation Alumni Tracking System';
 	<div class="row">
             <div class="col-lg-4">
                 <h2>SM Development Corporation</h2>
-				<img src = "images/alumni1.jpg" height="150px" width="250px">
+				<img src = "images/smdevco.jpg" height="150px" width="250px">
                 <p>is a fast-growing residential property developer focusing on the premium middle market, 
 				expertly balancing key housing factors of quality, style, durability, and affordability. </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>Highlands Prime, Inc.</h2>
-				<img src = "images/news.jpg" height="150px" width="250px">
+				<img src = "images/highlands.jpg" height="150px" width="250px">
                 <p> is the leading high-end residential developer within Tagaytay Highlands, an exclusive, world-class mountainside resort and residential complex.  </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>Taal Vista Hotel</h2>
-				<img src = "images/calendar.jpg" height="150px" width="250px">
+				<img src = "images/taal.jpg" height="150px" width="250px">
                 <p>  is a landmark hotel in Tagaytay City, offering gracious amenities that seamlessly blend with natural 
 				wonders such as the breathtaking view of pristine and scenic Taal Lake and Volcano.  </p>
 
@@ -89,7 +89,7 @@ $this->title = 'SM Foundation Alumni Tracking System';
 		<div class="row">
             <div class="col-lg-4">
                 <h2>SM BayCity</h2>
-				<img src = "images/alumni1.jpg" height="150px" width="250px">
+				<img src = "images/smbay.jpg" height="150px" width="250px">
                 <p> is an alternative business and tourism district, and is the home of the world's third largest mall, <br>
 				the SM Mall of Asia. SM Bay City occupies 
 				60 hectares of prime land along the coast of scenic Manila Bay, offering choice locations for business, tourism, retail, and leisure projects.  </p>
@@ -97,14 +97,14 @@ $this->title = 'SM Foundation Alumni Tracking System';
             </div>
             <div class="col-lg-4">
                 <h2>Hamilo Coast</h2>
-				<img src = "images/news.jpg" height="150px" width="250px">
+				<img src = "images/hamilo.jpg" height="150px" width="250px">
                 <p> is a unique large-scale ecotourism project to develop in phases 5,700 hectares of land by the sea in Nasugbu, 
 				Batangas into an innovative, integrated network of coastal resort communities. </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>SM Foundation, Inc.</h2>
-				<img src = "images/calendar.jpg" height="150px" width="250px">
+				<img src = "images/smf2.jpg" height="150px" width="250px">
                 <p>  is the corporate social responsibility arm of the SM Group. 
 				It focuses on four major programs and areas of advocacy, namely, education; 
 				religious; outreach; and health. </p>
@@ -116,25 +116,20 @@ $this->title = 'SM Foundation Alumni Tracking System';
 	<div class="row">
             <div class="col-lg-4">
                 <h2>Storyland</h2>
-				<img src = "images/alumni1.jpg" height="150px" width="250px">
-                <p> is an alternative business and tourism district, and is the home of the world's third largest mall, <br>
-				the SM Mall of Asia. SM Bay City occupies 
-				60 hectares of prime land along the coast of scenic Manila Bay, offering choice locations for business, tourism, retail, and leisure projects.  </p>
+				<img src = "images/smstoryland.jpg" height="150px" width="250px">
+                <p>  </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>SM Shoemart</h2>
-				<img src = "images/news.jpg" height="150px" width="250px">
-                <p> is a unique large-scale ecotourism project to develop in phases 5,700 hectares of land by the sea in Nasugbu, 
-				Batangas into an innovative, integrated network of coastal resort communities. </p>
+				<img src = "images/smshoemart.jpg" height="150px" width="250px">
+                <p> </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>SM Appliance Center</h2>
-				<img src = "images/calendar.jpg" height="150px" width="250px">
-                <p>  is the corporate social responsibility arm of the SM Group. 
-				It focuses on four major programs and areas of advocacy, namely, education; 
-				religious; outreach; and health. </p>
+				<img src = "images/smappcenter.jpg" height="150px" width="250px">
+                <p> </p>
 
             </div>
         </div>
@@ -143,25 +138,20 @@ $this->title = 'SM Foundation Alumni Tracking System';
 		<div class="row">
             <div class="col-lg-4">
                 <h2>SM Advantage</h2>
-				<img src = "images/alumni1.jpg" height="150px" width="250px">
-                <p> is an alternative business and tourism district, and is the home of the world's third largest mall, <br>
-				the SM Mall of Asia. SM Bay City occupies 
-				60 hectares of prime land along the coast of scenic Manila Bay, offering choice locations for business, tourism, retail, and leisure projects.  </p>
+				<img src = "images/smadvantage.jpg" height="150px" width="250px">
+                <p> </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>SM Cinema</h2>
-				<img src = "images/news.jpg" height="150px" width="250px">
-                <p> is a unique large-scale ecotourism project to develop in phases 5,700 hectares of land by the sea in Nasugbu, 
-				Batangas into an innovative, integrated network of coastal resort communities. </p>
+				<img src = "images/smcinema.jpg" height="150px" width="250px">
+                <p> </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>Megatrade Hall</h2>
-				<img src = "images/calendar.jpg" height="150px" width="250px">
-                <p>  is the corporate social responsibility arm of the SM Group. 
-				It focuses on four major programs and areas of advocacy, namely, education; 
-				religious; outreach; and health. </p>
+				<img src = "images/megatrade.jpg" height="150px" width="250px">
+                <p> </p>
 
             </div>
         </div>
@@ -169,26 +159,15 @@ $this->title = 'SM Foundation Alumni Tracking System';
 		
 		<div class="row">
             <div class="col-lg-4">
-                <h2>Storyland</h2>
-				<img src = "images/alumni1.jpg" height="150px" width="250px">
-                <p> is an alternative business and tourism district, and is the home of the world's third largest mall, <br>
-				the SM Mall of Asia. SM Bay City occupies 
-				60 hectares of prime land along the coast of scenic Manila Bay, offering choice locations for business, tourism, retail, and leisure projects.  </p>
-
-            </div>
-            <div class="col-lg-4">
                 <h2>Toy World</h2>
-				<img src = "images/news.jpg" height="150px" width="250px">
-                <p> is a unique large-scale ecotourism project to develop in phases 5,700 hectares of land by the sea in Nasugbu, 
-				Batangas into an innovative, integrated network of coastal resort communities. </p>
+				<img src = "images/toyworld.jpg" height="150px" width="250px">
+                <p> </p>
 
             </div>
             <div class="col-lg-4">
                 <h2>Toy Kingdom</h2>
-				<img src = "images/calendar.jpg" height="150px" width="250px">
-                <p>  is the corporate social responsibility arm of the SM Group. 
-				It focuses on four major programs and areas of advocacy, namely, education; 
-				religious; outreach; and health. </p>
+				<img src = "images/toykingdom.jpg" height="150px" width="250px">
+                <p> </p>
 
             </div>
         </div>
