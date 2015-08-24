2721316e09217ac23839773595b2fe4a0e8364f9
jlarroyo <jayson.arroyo36@gmail.com>
2015-08-24 23:28:24 +0800

    added widget file input

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/upload/_form.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/upload/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use dosamigos\fileinput\FileInput;
 /* @var $this yii\web\View */
 /* @var $model common\models\Upload */
 /* @var $form yii\widgets\ActiveForm */
@@ -21,6 +21,18 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'upload_form')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'upload_file_name')->textInput(['maxlength' => true]) ?>
+
+	<?= $form->field($model, 'file')->widget(\dosamigos\fileinput\BootstrapFileInput::className(), [
+    'options' => ['multiple' => true],
+    'clientOptions' => [
+        'previewFileType' => 'text',
+        'browseClass' => 'btn btn-success',
+        'uploadClass' => 'btn btn-info',
+        'removeClass' => 'btn btn-danger',
+        'removeIcon' => '<i class="glyphicon glyphicon-trash"></i> '
+    ],
+	
+])->label('Upload Form')?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
