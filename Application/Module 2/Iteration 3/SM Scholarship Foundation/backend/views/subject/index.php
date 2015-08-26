ba907575924b0e1647938f54ecc2e499d6277394
jlarroyo <jayson.arroyo36@gmail.com>
2015-08-27 02:19:19 +0800

    added tab buttons for index and index2

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/subject/index.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/subject/index.php
@@ -11,7 +11,7 @@ use common\models\SubjectSearch;
 /* @var $searchModel common\models\ScholarSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Scholars';
+$this->title = 'Subject';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="scholar-index">
@@ -19,9 +19,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Scholar', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
 <?php
 $exportedValues = 
 [            
@@ -43,71 +40,35 @@ $exportedValues =
 	],
 	'scholar_id',
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_first_name',
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_middle_name',
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_last_name',
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_gender',
-		'editableOptions' => [
-			'inputType' => 'dropDownList',
-			'pluginOptions'=>['allowClear'=>true],
-			'data' => ["Male"=>"Male","Female"=>"Female"],
-			'widgetClass'=> 'kartik\select2\Select2',
-		],
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_course',
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute'=>'school_school_id',
-		'editableOptions' => [
-			'inputType' => '\kartik\select2\Select2',
-			'options'=>
-			[
-				'data' => ArrayHelper::map(School::find()->all(),'school_id','school_name'),
-			],
-		],
 		'value'=>'schoolSchool.school_name',
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_year_level',
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_graduate_status',
-		'editableOptions' => [
-			'inputType' => 'dropDownList',
-			'pluginOptions'=>['allowClear'=>true],
-			'data' => ["Not Graduated"=>"Not Graduated","Graduated"=>"Graduated"],
-			'widgetClass'=> 'kartik\select2\Select2',
-		],
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_vendor_code',
 	],
 	[
-		'class' => 'kartik\grid\EditableColumn',
 		'attribute' => 'scholar_type',
-		'editableOptions' => [
-			'inputType' => 'dropDownList',
-			'pluginOptions'=>['allowClear'=>true],
-			'data' => ["SMFI"=>"SMFI","My Scholar A"=>"My Scholar A","Kabayan Scholar"=>"Kabayan Scholar",
-			"Kabayan Scholar"=>"Kabayan Scholar","My Scholar B"=>"My Scholar B","ICA Grant Scholar"=>"ICA Grant Scholar"],
-			'widgetClass'=> 'kartik\select2\Select2',
-		],
 	],
 
 	['class' => 'kartik\grid\ActionColumn'],
@@ -129,6 +90,8 @@ $export = ExportMenu::widget([
         ]
 	]);
 ?>
+<?= Html::a('Group By Scholar', ['index'], ['class' => 'btn btn-success']) ?>
+<?= Html::a('Show Only Subject Records', ['index2'], ['class' => 'btn btn-success']) ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -146,7 +109,7 @@ $export = ExportMenu::widget([
         'columns' => $exportedValues,
 		'toolbar'=> [
 			[
-				'content'=>Html::a('Create Scholar', ['create'], ['class' => 'btn btn-success'])
+				'content'=>Html::a('Create Subject', ['create'], ['class' => 'btn btn-success'])
 			],
   //     	'{export}',
 			'{toggleData}',
