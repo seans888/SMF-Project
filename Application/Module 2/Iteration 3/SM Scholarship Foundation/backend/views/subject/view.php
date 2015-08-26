23660e1e0fb40877ce1e4484247fc2f53c3db6e4
jlarroyo <jayson.arroyo36@gmail.com>
2015-08-27 02:18:38 +0800

    changed the parameters passed to view buttons

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/subject/view.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/subject/view.php
@@ -15,8 +15,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'subject_id' => $model->subject_id, 'scholar_scholar_id' => $model->scholar_scholar_id, 'scholar_school_school_id' => $model->scholar_school_school_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'subject_id' => $model->subject_id, 'scholar_scholar_id' => $model->scholar_scholar_id, 'scholar_school_school_id' => $model->scholar_school_school_id], [
+        <?= Html::a('Update', ['update', 'id' => $model->subject_id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->subject_id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
