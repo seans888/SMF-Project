d04da94276e52551d1c60f2c2020d05c0ba5b61c
jgvillacorta <jkevinvillacorta@gmail.com>
2015-08-27 01:04:41 +0800

    grade records and subject list tab

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/frontend/views/grade/index.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/frontend/views/grade/index.php
@@ -4,6 +4,11 @@ use yii\helpers\Html;
 use kartik\grid\GridView;
 use kartik\tabs\TabsX;
 use yii\helpers\Url;
+use common\models\Subject;
+use common\models\Scholar;
+use common\models\Grade;
+use common\models\GradeSearch;
+use common\models\SubjectSearch;
 
 
 /* @var $this yii\web\View */
@@ -16,31 +21,59 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="grade-index">
 
     <center><h1 style="margin-top:100px;"><?= Html::encode($this->title) ?></h1></center><br>
+	<?php 
+	 
+	ob_start();
+			$model = new Subject();
+            $searchModel = new SubjectSearch();
+            $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+            echo $this->render('/subject/index',[
+                'model'=>$model,
+                'dataProvider'=>$dataProvider,
+                'searchModel'   =>$searchModel,
+            ]);
+ 
+	$TabContent=ob_get_clean();
+	
+	?>
   <?php 
    $items = [
+   
+    
     [
         'label'=>'<i class="glyphicon glyphicon-list-alt"></i> Grade Records',
         'content'=> GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-		'showOnEmpty' => false,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-            'grade_school_year_start',
-            'grade_school_year_end',
-         
 
             
+            'scholar_scholar_id',
+            'subject_term',
+            'subject_name',
+		
+            // 'subject_units',
+            // 'subject_taken_status',
+            // 'subject_approval_status',
+            // 'subject_approved_by',
+
+            ['class' => 'yii\grid\ActionColumn'],
         ],
-    ]), 
+    ]),
                    'active' => true,
        
+    ],
+	[
+        'label'=>'<i class="glyphicon glyphicon-pencil"></i> Input Grades',
+        'content'=>$this->render('groupcreate'),
     ],
     [
         'label'=>'<i class="glyphicon glyphicon-thumbs-down"></i> Low/Fail Grade Explanation Form',
         'content'=>'<p style="color:red"><i>NOTICE:If the computed grade/s is below 85% or below 70% (5.0 or 0.0), Please answer the question below to be directly submitted to SM Foundation</i>.</p><br>Please explain the reason for attaining low or fail grade/s. Explain the effect of the deficiency. Will this affect the length of time you graduate?. Explain how will you correct the deficiency.<br><br><textarea rows=10 cols=150></textarea>
 			<button class="btn btn-success" style="margin-bottom:40px;">Submit</button><br><br><h5><i>The explanation will be reviewed by the SM Foundation. <br>We will inform you for any updates regarding the case. Thank You! </i></h5>',
     ],
+	
     
 ];
 // Ajax Tabs Above
