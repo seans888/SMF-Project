54c390ea475aea4cd4115e6cd88547bfddb5d28c
sudaswani <syam090311@gmail.com>
2015-08-27 15:07:08 +0800

    added editable column and modified exported values

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/tuition/index2.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/tuition/index2.php
@@ -3,10 +3,76 @@
 use yii\helpers\Html;
 use kartik\grid\GridView;
 use kartik\export\ExportMenu;
+use common\models\Scholar;
+use yii\helpers\ArrayHelper;
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\TuitionSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
+$gridColumns = 
+[            
+	['class' => 'kartik\grid\SerialColumn'],
+	[
+		'class' => 'kartik\grid\EditableColumn',
+		'attribute'=>'scholar_scholar_id',
+		'editableOptions' => [
+			'inputType' => '\kartik\select2\Select2',
+			'options'=>
+			[
+				'data' => ArrayHelper::map(Scholar::find()->all(),'scholar_id','scholar_last_name','scholar_id'),
+			],
+		],
+	],
+	[
+		'attribute' => 'firstName',
+		'value' => 'scholarScholar.scholar_first_name'
+	],
+	[
+		'attribute' => 'middleName',
+		'value' => 'scholarScholar.scholar_middle_name'
+	],
+	[
+		'attribute' => 'lastName',
+		'value' => 'scholarScholar.scholar_last_name'
+	],
+	[
+		'class' => 'kartik\grid\EditableColumn',
+		'attribute' => 'tuition_term',
+	],
+	// [
+		// 'class' => 'kartik\grid\EditableColumn',
+		// 'attribute' => 'tuition_school_year_start',
+	// ],
+	// [
+		// 'class' => 'kartik\grid\EditableColumn',
+		// 'attribute' => 'tuition_school_year_end',
+	// ],
+	[
+		'class' => 'kartik\grid\EditableColumn',
+		'attribute' => 'tuition_enrollment_date',
+	],
+	[
+		'class' => 'kartik\grid\EditableColumn',
+		'attribute' => 'tuition_amount',
+		'pageSummary' => true
+	],
+	[
+		'class' => 'kartik\grid\EditableColumn',
+		'attribute' => 'tuition_paid_status',
+		'editableOptions' => [
+			'inputType' => 'dropDownList',
+			'pluginOptions'=>['allowClear'=>true],
+			'data' => ["Not Paid"=>"Not Paid","Paid"=>"Paid"],
+			'widgetClass'=> 'kartik\select2\Select2',
+		],
+	],
+	// [
+		// 'class' => 'kartik\grid\EditableColumn',
+		// 'attribute' => 'tuition_payment_date',
+		// 'pageSummary' => true
+	// ],
 
+	['class' => 'kartik\grid\ActionColumn'],
+];
 $exportedValues = 
 [            
 	['class' => 'kartik\grid\SerialColumn'],
@@ -42,7 +108,7 @@ $exportedValues =
 $export = ExportMenu::widget([
 		'dataProvider' => $dataProvider,
         'columns' => $exportedValues,
-		'noExportColumns'=>[11],
+		'noExportColumns'=>[13],
         'columnSelectorOptions'=>[
             'label' => 'Columns',
             'class' => 'btn btn-danger'
@@ -70,7 +136,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
 		'pjax' => true,
 		'pjaxSettings' => ['neverTimeout' => true],
-        'columns' => $exportedValues,
+        'columns' => $gridColumns,
 		'toolbar'=> [
         ['content'=>Html::a('Create Tuition', ['groupcreate'], ['class' => 'btn btn-success'])
         ],
