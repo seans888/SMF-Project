7bae5081d6a07d1b9e1d68b3a0526c7a6b4288b0
sudaswani <syam090311@gmail.com>
2015-08-27 11:50:11 +0800

    added drop down expansion

---
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/deduction/_dropdown.php
@@ -0,0 +1,72 @@
+<?php
+
+use yii\helpers\Html;
+use kartik\grid\GridView;
+use kartik\export\ExportMenu;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\IncentiveSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Incentives';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<?php // echo $this->render('_search', ['model' => $searchModel]);
+$exportedValues =
+	[
+		['class' => 'kartik\grid\SerialColumn'],
+		
+            'deduction_date',
+            'deduction_amount',
+		
+		['class' => 'kartik\grid\ActionColumn'],
+	];
+	
+	$export = ExportMenu::widget([
+			'dataProvider' => $dataProvider,
+			'columns' => $exportedValues,
+			'noExportColumns' => [0,3],
+			'columnSelectorOptions'=>[
+				'label' => 'Columns',
+				'class' => 'btn btn-danger'
+				],
+				'target' => '_blank',
+				'fontAwesome' => true,
+				'dropdownOptions' => [
+					'label' => 'Export',
+					'class' => 'btn btn-success'
+					]
+				]);
+				
+?>
+				
+<div class="incentive-index">
+
+	<?php ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+		'pjax' => true,
+		'pjaxSettings' => 
+		[
+			'neverTimeout' => true
+		],
+        'columns' => $exportedValues,
+		// 'toolbar'=> [
+		// [
+			// 'content'=>html::a('Create Incentive', ['create'], ['class' => 'btn btn-success'])
+		// ],
+		
+		// '{toggleData}',
+		// $export
+		// ],
+		// 'panel'=>
+		// [
+			// 'type'=>GridView::TYPE_PRIMARY,
+			// 'heading'=>'Incentive Table',
+		// ]
+    ]); 
+	?>
+
+</div>
