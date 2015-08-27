ca7a55fc80a56b37eec6f51add6da3f7bc6e2932
sudaswani <syam090311@gmail.com>
2015-08-27 16:36:20 +0800

    added date picker widget

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/deduction/_form.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/deduction/_form.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use common\models\Scholar;
+use common\models\School;
+use dosamigos\datepicker\DatePicker;
 use yii\helpers\ArrayHelper;
 use kartik\select2\Select2;
 /* @var $this yii\web\View */
@@ -22,9 +24,25 @@ use kartik\select2\Select2;
 			'pluginOptions'=>['allowClear'=>true],
 		]) ?>
 
-    <?= $form->field($model, 'scholar_school_school_id')->textInput() ?>
+    <?= $form->field($model,'scholar_school_school_id')->widget(Select2::classname(),
+		[
+			'data'=>ArrayHelper::map(School::find()->all(),'school_id','school_name'),
+			'language'=>'en',
+			'options'=>['placeholder'=>'Select School'],
+			'pluginOptions'=>['allowClear'=>true],
+		]) ?>
 
-    <?= $form->field($model, 'deduction_date')->textInput() ?>
+    <?= $form->field($model, 'deduction_date')->widget(
+			DatePicker::className(), [
+				// inline too, not bad
+				 'inline' => false, 
+				 // modify template for custom rendering
+			//	'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+				'clientOptions' => [
+					'autoclose' => true,
+					'format' => 'yyyy-mm-dd'
+				]
+			]);?>
 
     <?= $form->field($model, 'deduction_amount')->textInput(['maxlength' => true]) ?>
 
