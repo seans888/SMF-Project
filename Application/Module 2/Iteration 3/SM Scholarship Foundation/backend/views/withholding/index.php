346bf3fb5777ea4e308c9fca31d5b580f2150876
sudaswani <syam090311@gmail.com>
2015-08-30 18:29:32 +0800

    modified index.php to render scholar records

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/withholding/index.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/withholding/index.php
@@ -1,40 +1,138 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
-
+use kartik\grid\GridView;
+use kartik\export\ExportMenu;
+use yii\helpers\ArrayHelper;
+use kartik\select2\Select2;
+use common\models\School;
+use common\models\Withholdingsearch;
 /* @var $this yii\web\View */
-/* @var $searchModel common\models\WithholdingSearch */
+/* @var $searchModel common\models\ScholarSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Withholdings';
+$this->title = 'Withholding';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="withholding-index">
+<div class="scholar-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Withholding', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+<?php
+$exportedValues = 
+[            
+	[
+			'class' => 'kartik\grid\ExpandRowColumn',
+			'value' => function($model, $key, $index, $column){
+				return GridView::ROW_COLLAPSED;
+			},
+			'detail' => function ($model, $key, $index, $column){
+				$searchModel = new WithholdingSearch();
+				$searchModel -> scholar_scholar_id = $model->scholar_id;
+				$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+				
+				return Yii::$app->controller->renderPartial('_dropdown',[
+					'searchModel' => $searchModel,
+					'dataProvider' => $dataProvider,
+				]);
+			},
+	],
+	'scholar_id',
+	[
+		'attribute' => 'scholar_first_name',
+	],
+	[
+		'attribute' => 'scholar_middle_name',
+	],
+	[
+		'attribute' => 'scholar_last_name',
+	],
+	[
+		'attribute' => 'school_school_id',
+		'value' => 'schoolSchool.school_name'
+	],
+	// [
+		// 'attribute' => 'scholar_gender',
+	// ],
+	// [
+		// 'attribute' => 'scholar_course',
+	// ],
+	// [
+		// 'attribute'=>'school_school_id',
+		// 'value'=>'schoolSchool.school_name',
+	// ],
+	// [
+		// 'attribute' => 'scholar_year_level',
+	// ],
+	// [
+		// 'attribute' => 'scholar_graduate_status',
+	// ],
+	// [
+		// 'attribute' => 'scholar_vendor_code',
+	// ],
+	// [
+		// 'attribute' => 'scholar_type',
+	// ],
+];
 
+$export = ExportMenu::widget([
+		'dataProvider' => $dataProvider,
+        'columns' => $exportedValues,
+		'noExportColumns'=>[18],
+        'columnSelectorOptions'=>[
+            'label' => 'Columns',
+            'class' => 'btn btn-danger'
+        ],
+		'target' => '_blank',
+        'fontAwesome' => true,
+        'dropdownOptions' => [
+            'label' => 'Export',
+            'class' => 'btn btn-success'
+        ]
+	]);
+?>
+<?= Html::a('Group By Scholar', ['index'], ['class' => 'btn btn-success']) ?>
+<?= Html::a('Show Only Withholding Records', ['index2'], ['class' => 'btn btn-success']) ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'withholding_id',
-            'scholar_scholar_id',
-            'scholar_school_school_id',
-            'scholar_allowance_allowance_area',
-            'withholding_start_date',
-            // 'withholding_end_date',
-            // 'withholding_remark',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+		'pjax' => true,
+		'pjaxSettings' => 
+		[
+			'neverTimeout' => true
+		],
+    	'rowOptions'=>function($model){
+    		if(strcasecmp($model->allowance_allowance_area, 'NCR') != 0)
+    		{
+    			return['class'=>'provincial-row'];
+    		}
+    		else if(strcasecmp($model->allowance_allowance_area, 'NCR') == 0)
+    		{
+    			return['class'=>'ncr-row'];
+    		}
+    	},
+		'pjax' => true,
+        'columns' => $exportedValues,
+		'toolbar'=> [
+			[
+				'content'=>Html::a('Create Withholding Record', ['create'], ['class' => 'btn btn-success'])
+			],
+  //     	'{export}',
+			'{toggleData}',
+			// $export
+		],
+ //   set export properties
+    'export'=>[
+        'fontAwesome'=>true,
+		'label' => 'Export',
+		'target' => '_blank'
+    ],
+		'panel'=>[
+        'type'=>GridView::TYPE_PRIMARY,
+        'heading'=>'Scholar List',
+    ],
+    ]); 
+	?>
 
 </div>
