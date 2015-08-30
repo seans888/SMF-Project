c88a2f8dbfc3519d74a496eaa71a7f72c126ece7
sudaswani <syam090311@gmail.com>
2015-08-30 15:36:10 +0800

    added link for withholding allowance records

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/layouts/main.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/layouts/main.php
@@ -130,6 +130,7 @@ DashboardAsset::register($this);
               </a>
               <ul class="treeview-menu">
 				<li><a href="<?= Yii::$app->getUrlManager()->createUrl('/allowance/index'); ?>"><i class="fa fa-credit-card"></i> Allowance Reference Table</a></li>
+				<li><a href="<?= Yii::$app->getUrlManager()->createUrl('/withholding/index'); ?>"><i class="fa fa-thumbs-down"></i> Withholding</a></li>
 				<li><a href="<?= Yii::$app->getUrlManager()->createUrl('/incentive/index'); ?>"><i class="fa fa-thumbs-up"></i> Incentives</a></li>
 				<li><a href="<?= Yii::$app->getUrlManager()->createUrl('/deduction/index'); ?>"><i class="fa fa-thumbs-down"></i> Deductions</a></li>
 				</ul>
