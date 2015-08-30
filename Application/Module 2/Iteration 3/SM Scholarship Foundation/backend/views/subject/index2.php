7c6bb37243b2d5c79dcc6ca17bb05a0a0b2ef98a
sudaswani <syam090311@gmail.com>
2015-08-30 13:38:42 +0800

    added fields for scholar's name

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/subject/index2.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/subject/index2.php
@@ -10,7 +10,22 @@ use common\models\Subject;
 $exportedValues =
 [
             ['class' => 'kartik\grid\SerialColumn'],
-			
+			[
+				'class' => 'kartik\grid\EditableColumn',
+				'attribute' => 'scholar_scholar_id',
+			],
+			[
+				'attribute' => 'firstName',
+				'value' => 'scholarScholar.scholar_first_name'
+			],
+			[
+				'attribute' => 'middleName',
+				'value' => 'scholarScholar.scholar_middle_name'
+			],
+			[
+				'attribute' => 'lastName',
+				'value' => 'scholarScholar.scholar_last_name'
+			],
 			[
 				'class' => 'kartik\grid\EditableColumn',
 				'attribute' => 'subject_term',
