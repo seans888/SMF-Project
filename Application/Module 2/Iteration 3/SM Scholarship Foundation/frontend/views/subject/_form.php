b59274b1f72a3cd3b27f589db276befaabd0aca9
jgvillacorta <jkevinvillacorta@gmail.com>
2015-09-03 15:10:31 +0900

    deduction and incentive tab

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/frontend/views/subject/_form.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/frontend/views/subject/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use common\models\Grade;
 use common\models\Subject;
+
 /* @var $this yii\web\View */
 /* @var $model common\models\Subject */
 /* @var $form yii\widgets\ActiveForm */
@@ -19,20 +20,8 @@ use common\models\Subject;
 
     <?= $form->field($model, 'subject_units')->textInput(['maxlength' => true]) ?>
 	
-	<?php 
-	ob_start();
-			$model = new Grade();
-           if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['index', 'id' => $model->grade_id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
- 
-	ob_get_clean();
-	?>
-	<?= $form->field($model, 'grade_raw_grade')->textInput(['maxlength' => true]) ?>
+
+	
 
 
     <div class="form-group">
