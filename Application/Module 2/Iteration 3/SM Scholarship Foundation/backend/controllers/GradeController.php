2f7be4af3247196d14e12107edf13e113293ad11
sudaswani <syam090311@gmail.com>
2015-09-04 21:27:15 +0800

    fixed minor error of unknown field

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/controllers/GradeController.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/controllers/GradeController.php
@@ -237,10 +237,10 @@ class GradeController extends Controller
 
         if ($model->load(Yii::$app->request->post())) {
 			$selectSchool = ArrayHelper::map(Scholar::find()
-			->where(['scholar_id'=>$model->scholar_scholar_id])
+			->where(['scholar_id'=>$model->subject_scholar_scholar_id])
 			->all(),'school_school_id','school_school_id');
 			$schoolID = array_values($selectSchool)[0];
-			$model->scholar_school_school_id = $schoolID;
+			$model->subject_scholar_school_school_id = $schoolID;
 			
 			$model->save();
             return $this->redirect(['view', 'id' => $model->grade_id]);
