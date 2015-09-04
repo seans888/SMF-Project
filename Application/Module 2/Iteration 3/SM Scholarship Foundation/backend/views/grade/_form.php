f2204b72ed8495c229006d0d3ee82ae95de5104e
sudaswani <syam090311@gmail.com>
2015-09-04 21:19:11 +0800

    modified form for update and added widgets

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/grade/_form.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/backend/views/grade/_form.php
@@ -2,7 +2,10 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use kartik\select2\Select2;
+use common\models\Subject;
+use yii\helpers\ArrayHelper;
+use common\models\Scholar;
 /* @var $this yii\web\View */
 /* @var $model common\models\Grade */
 /* @var $form yii\widgets\ActiveForm */
@@ -11,14 +14,22 @@ use yii\widgets\ActiveForm;
 <div class="grade-form">
 
     <?php $form = ActiveForm::begin(); ?>
+	
+	<?= $form->field($model,'subject_subject_id')->widget(Select2::classname(),
+		[
+			'data'=>ArrayHelper::map(Subject::find()->all(),'subject_id','subject_name'),
+			'language'=>'en',
+			'options'=>['placeholder'=>'Select Scholar ID'],
+			'pluginOptions'=>['allowClear'=>true],
+		]) ?>
 
-    <?= $form->field($model, 'grade_id')->textInput() ?>
-
-    <?= $form->field($model, 'subject_subject_id')->textInput() ?>
-
-    <?= $form->field($model, 'subject_scholar_scholar_id')->textInput() ?>
-
-    <?= $form->field($model, 'subject_scholar_school_school_id')->textInput() ?>
+	<?= $form->field($model,'subject_scholar_scholar_id')->widget(Select2::classname(),
+		[
+			'data'=>ArrayHelper::map(Scholar::find()->all(),'scholar_id','scholar_last_name','scholar_id'),
+			'language'=>'en',
+			'options'=>['placeholder'=>'Select Scholar ID'],
+			'pluginOptions'=>['allowClear'=>true],
+		]) ?>
 
     <?= $form->field($model, 'grade_school_year_start')->textInput() ?>
 
