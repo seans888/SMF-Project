5375b6e0633c97c272fbbf66117c3de69c47cc9d
jgvillacorta <jkevinvillacorta@gmail.com>
2015-09-08 01:11:05 +0900

    quality check changes

--- Application/Module 2/Iteration 3/SM Scholarship Foundation/frontend/views/optionalwork/update.php
+++ Application/Module 2/Iteration 3/SM Scholarship Foundation/frontend/views/optionalwork/update.php
@@ -5,14 +5,11 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Optionalwork */
 
-$this->title = 'Update Optionalwork: ' . ' ' . $model->optionalwork_id;
-$this->params['breadcrumbs'][] = ['label' => 'Optionalworks', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->optionalwork_id, 'url' => ['view', 'id' => $model->optionalwork_id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Update Summer/Christmas Job';
 ?>
 <div class="optionalwork-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 style="margin-top:100px;"><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
